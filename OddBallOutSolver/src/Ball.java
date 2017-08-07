926d2791ddb73837880f91c9227d6d4e5fc19cc7
Junno <Junno@10.0.1.220>
2017-08-08 07:44:48 +0800

    initial, not done

---
+++ OddBallOutSolver/src/Ball.java
@@ -0,0 +1,25 @@
+
+public class Ball {
+	public int id;
+	public int weight;
+	
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Ball(){
+		
+	}
+}
---
+++ OddBallOutSolver/src/OddBallOutSolver.java
@@ -0,0 +1,61 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class OddBallOutSolver {
+	public static int SIZE = 12;
+	
+	public String weightIn(LinkedList left, LinkedList right){
+		for(I){
+			
+		}
+		
+		if(left.equals(right)){
+			/*Do something*/
+			return "=";
+		}else if(){
+			/*Do something*/
+			return ">";
+		}else if(){
+			/*Do something*/
+			return "<";
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer, LinkedList> balls = new HashMap<Integer,LinkedList>();
+		for(int i=1; i<=SIZE; i++){
+			balls.put(i, new LinkedList());
+		}
+//		
+//		//balls.get(1).add(0,new LinkedList());
+//		for (Map.Entry<Integer, LinkedList> entry : balls.entrySet()) {
+//			//System.out.println(entry.getKey()-1);
+//			balls.get(entry.getKey()).add(new LinkedList());
+//			balls.get(entry.getKey()).add(new LinkedList());
+//			balls.get(entry.getKey()).add(new LinkedList());    
+//		}
+//			
+//		for (Map.Entry<Integer, LinkedList> entry : balls.entrySet()) {
+//			Iterator it = balls.get(entry.getKey()).iterator();
+//			while (it.hasNext()) {
+//			    LinkedList list = (LinkedList) it.next();
+//			    list.
+//			    list.add(new LinkedList());
+//			    list.add(new LinkedList());
+//			    list.add(new LinkedList());
+//				
+//			}
+//		}
+//		for (Map.Entry<Integer, LinkedList> entry : balls.entrySet()) {
+//			//System.out.println(entry.getKey()-1);
+//			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+//		}
+			
+		
+	}
+
+}
